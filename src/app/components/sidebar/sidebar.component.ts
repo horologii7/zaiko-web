ebfb60de664b62cf8a605755fbf943a120a62f19
horologii <ruanfelipedpontes@gmail.com>
2019-11-09 19:48:08 -0300

    Inicio do projeto

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Resumo',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'Vendas',  icon:'store', class: '' },
+    { path: '/stock', title: 'Estoque',  icon:'all_inbox', class: '' },
+    { path: '/table-list', title: 'Produtos',  icon:'shopping_basket', class: '' },
+    { path: '/icons', title: 'Relatórios',  icon:'content_paste', class: '' },
+    //{ path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    { path: '/upgrade', title: 'Configuração',  icon:'build', class: 'active-pro' }
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
